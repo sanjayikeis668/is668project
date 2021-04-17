6750700a027110b221ea6be4f3a8ecd72d89acf9
Ike <ikeeneremadu@yahoo.com>
2021-04-17 21:47:02 +0000

    Attempt #4

--- flask_app.py
+++ flask_app.py
@@ -34,7 +34,7 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main.html", comments=Comment.query.all())
 
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
