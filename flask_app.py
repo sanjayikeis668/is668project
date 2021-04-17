386859d992198f47034143d6c3a9a1af3e89ef7a
Ike <ikeeneremadu@yahoo.com>
2021-04-17 21:25:14 +0000

    Should be done rn

--- flask_app.py
+++ flask_app.py
@@ -4,8 +4,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect
-from flask import url_for
+
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -34,7 +33,7 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main.html", comments=Comment.query.all())
 
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
