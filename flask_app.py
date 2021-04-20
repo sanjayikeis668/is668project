d077ac25449d262a3c595d35417c977fa11f4b8c
Ike <ikeeneremadu@yahoo.com>
2021-04-20 18:32:46 +0000

    Completed db migrations

--- flask_app.py
+++ flask_app.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -26,6 +27,7 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.secret_key = "Ikechukwu"
 login_manager = LoginManager()
